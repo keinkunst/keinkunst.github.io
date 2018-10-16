e385e3ea2954d34563de0be6c16e27a9d04686df
keinkunst <einarsson.kr@gmail.com>
2018-10-16 19:55:19 +0000

    Uppfærði G1, G2, P0, P1, P2.

--- Prufur/sketch.js
+++ Prufur/sketch.js
@@ -1,14 +1,10 @@
-var capture;
-
-function setup() {
-  createCanvas(390, 240);
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  //capture.hide();
-}
-
-function draw() {
-  background(255);
-  image(capture, 0, 0, 320, 240);
-  filter('INVERT');
+function setup(){
+    createCanvas(300,300);
+    background(255,0,0);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(20);
+    text("Halló heimur",200,40);rect(200,200,10)
+ellise(100,200,50,50); 
 }
