811d7d36515e03b01a6863d1b3bfb5b8fb6831ec
Kristján <einarsson.kr@gmail.com>
2018-11-07 09:10:07 +0000

    up

--- SF2018/G5-bkgrImage/sketch.js
+++ SF2018/G5-bkgrImage/sketch.js
@@ -18,14 +18,18 @@ function setup() {
 
 function draw() {
   hlutfall = random(0.1,0.3);
+  tint(random(0,360),75,255);
+  imageMode(CENTER);
   image(elgur, random(0,width),random(0,height),elgur.width*hlutfall,elgur.height*hlutfall);
 }
 function mousePressed() {
   imageMode(CENTER);
+  noTint();
   image(rostungur,mouseX,mouseY);
 }
 
 function keyPressed() {
   imageMode(CORNER);
+  noTint();
   background(hundur);
 }
