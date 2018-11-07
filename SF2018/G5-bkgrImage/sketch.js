2058016b2a5f829e2c4f5f5bb42a0a3d7ee3d514
Kristján <einarsson.kr@gmail.com>
2018-11-07 08:58:07 +0000

    up

--- SF2018/G5-bkgrImage/sketch.js
+++ SF2018/G5-bkgrImage/sketch.js
@@ -1,8 +1,12 @@
 var rostungur;
+var elgur;
 var hundur;
+var hlutfall = 1;
+
 
 function preload() {
   rostungur = loadImage('assets/walrus.png');
+  elgur = loadImage('assets/moose.png');
   hundur = loadImage('assets/einhundingur.jpg');
 }
 
@@ -14,7 +18,8 @@ function setup() {
 
 function draw() {
   fill(random(0,360),75,255);
-  ellipse (random(0,width),random(0,height),10,10);
+  hlutfall = random(0.2,0.5);
+  image(elgur, random(0,width),random(0,height),elgur.width*hlutfall,elgur.height*hlutfall);
 }
 function mousePressed() {
   imageMode(CENTER);
