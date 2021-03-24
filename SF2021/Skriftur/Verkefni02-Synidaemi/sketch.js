2f6406e8578e772c7095e50c12836771743fdd11
Kristján <einarsson.kr@gmail.com>
2021-03-24 12:38:35 +0000

    Verkefni02

---
+++ SF2021/Skriftur/Verkefni02-Synidaemi/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400,400);
+  background(255, 215, 0);
+  colorMode(HSB);
+  frameRate(2);
+  strokeWeight(2);
+}
+
+function draw() {
+  for(var x = 10; x < width; x = x+20){
+    fill(random(180,360),75,100);
+    for(var y = 10; y < height; y = y+20){
+        ellipse(x, y, 15, 15);
+    }
+  }
+}
